319926b2bf698118cf24294ca7bcc66ccd653af8
Kian Metcalfe <kian@metcalfe.net>
2022-04-25 19:41:13 -0400

    Rent and buy payment logic parity

--- Server/api/Controller/Api/PaymentController.php
+++ Server/api/Controller/Api/PaymentController.php
@@ -11,43 +11,7 @@ require '/joyride/api/Model/PaymentModel.php';
 
 class PaymentController extends BaseController {
   public function buyIntentAction() {
-    $strErrorDesc = '';
-    $requestMethod = $_SERVER["REQUEST_METHOD"];
-    $arrQueryStringParams = $this->getQueryStringParams();
-
-    if (strtoupper($requestMethod) == 'POST') {
-      try {
-        $data = file_get_contents('php://input');
-        $body = json_decode($data);
-        $price = $body->{'cents'};
-
-        $paymentIntent = \Stripe\PaymentIntent::create([
-          'amount' => $price,
-          'currency' => 'usd',
-          'automatic_payment_methods' => [
-            'enabled' => true
-          ]
-        ]);
-
-        $responseData = '{"clientSecret":"' . $paymentIntent->client_secret . '"}';
-      } catch (Error $e) {
-        $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
-        $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
-      }
-    }
-
-    // send output
-    if (!$strErrorDesc) {
-      $this->sendOutput(
-        $responseData,
-        array('Content-Type: application/json', 'HTTP/1.1 200 OK')
-      );
-    } else {
-      $this->sendOutput(
-        json_encode(array('error' => $strErrorDesc)),
-        array('Content-Type: application/json', $strErrorHeader)
-      );
-    }
+    $this->rentIntentAction();
   }
 
   public function rentIntentAction() {
